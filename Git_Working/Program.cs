0bcbbf3fed8b73b933b817d28e635489cd1dd285
Jeremy Bivens <jerbbive@ut.utm.edu>
2019-09-04 21:37:22 -0500

    User pick random generator bounds.

--- Git_Working/Program.cs
+++ Git_Working/Program.cs
@@ -11,18 +11,31 @@ namespace Git_Working
         static void Main(string[] args)
         {
             int int_choice;
+            int int_min;
+            int int_max;
+
+            
+            
+
+            Console.WriteLine("Please pick a low number.");
+            string min = Console.ReadLine();
+            int.TryParse(min, out int_min);
+
+            Console.WriteLine("Please pick a high number.");
+            string max = Console.ReadLine();
+            int.TryParse(max, out int_max);
 
             Random random = new System.Random();
-            int num = random.Next(0, 100);
-
-            Console.WriteLine("Hello World!");
+            int num = random.Next(int_min, int_max);
             Console.WriteLine(num);
 
             Console.WriteLine("Please guess a number.");
             string choice = Console.ReadLine();
             int.TryParse(choice, out int_choice);
 
-            while(int_choice != num)
+            
+
+            while (int_choice != num)
             {
                 if(int_choice > num)
                 {
@@ -39,7 +52,7 @@ namespace Git_Working
             Console.WriteLine("You Guessed Correctly!");
 
             
-
+            
 
 
             Console.WriteLine("Push any key to exit.");
