959c91f4cba6ff5f1d4f91d8e3b037ae94bec56f
Jeremy Bivens <jerbbive@ut.utm.edu>
2019-09-04 21:14:31 -0500

    Number Generater and Guesser Working

--- Git_Working/Program.cs
+++ Git_Working/Program.cs
@@ -10,7 +10,37 @@ namespace Git_Working
     {
         static void Main(string[] args)
         {
+            int int_choice;
+
+            Random random = new System.Random();
+            int num = random.Next(0, 100);
+
             Console.WriteLine("Hello World!");
+            Console.WriteLine(num);
+
+            Console.WriteLine("Please guess a number.");
+            string choice = Console.ReadLine();
+            int.TryParse(choice, out int_choice);
+
+            while(int_choice != num)
+            {
+                if(int_choice > num)
+                {
+                    Console.WriteLine("Guess too high.");
+                }
+                else
+                {
+                    Console.WriteLine("Guess too low");
+                }
+                Console.WriteLine("Please another guess a number.");
+                choice = Console.ReadLine();
+                int.TryParse(choice, out int_choice);
+            }
+            Console.WriteLine("You Guessed Correctly!");
+
+            
+
+
 
             Console.WriteLine("Push any key to exit.");
             Console.ReadKey();
